7292e09e3a48a67e5b2e6464c07d9cdc4a0895a7
Lyga17 <nick@buildrr.com>
2013-09-10 11:08:42 -0400

    removed unecessary commented out code

--- Model/CoursesAppModel.php
+++ Model/CoursesAppModel.php
@@ -4,32 +4,5 @@ class CoursesAppModel extends AppModel {
 	
 	public $actsAs = array('Themeable');
 	
-/**
- * Constructor
- *
- * Added media.attachable here so its available to all children Models
- * 
- * @return void
-	public function __construct($id = false, $table = null, $ds = null) {
-		if(CakePlugin::loaded('Media')) {
-			$this->actsAs[] = 'Media.MediaAttachable';
-			
-			$this->hasMany['Media'] = array(
-				'className' => 'Media.Media',
-				'foreignKey' => 'foreign_key',
-				'dependent' => false, // Incase Media is attached to more that one model
-				'conditions' => '',
-				'fields' => '',
-				'order' => '',
-				'limit' => '',
-				'offset' => '',
-				'exclusive' => '',
-				'finderQuery' => '',
-				'counterQuery' => ''
-			);
-		}
-		parent::__construct($id, $table, $ds); 
-	}
-	
- */ 
+
 }
