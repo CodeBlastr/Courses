69cb360913234b55d21542850df2429ff01c8274
Joel Byrnes <joel@razorit.com>
2014-02-28 12:05:19 -0500

    removes whitespace & formats code

--- Model/CourseGradeDetail.php
+++ Model/CourseGradeDetail.php
@@ -1,14 +1,15 @@
 <?php
+
 App::uses('CoursesAppModel', 'Courses.Model');
+
 /**
  * Course Grade Detail Model - Used to store grading details for each grade given.
  *
  * @property Course $ParentCourse
  */
 class CourseGradeDetail extends CoursesAppModel {
-	
+
 	public $name = 'CourseGradeDetail';
-	
 	public $useTable = 'course_grade_details';
 
 /**
@@ -32,7 +33,6 @@ class CourseGradeDetail extends CoursesAppModel {
 			'order' => ''
 		),
 	);
-	
 	public $hasMany = array(
 		'GradeAnswers' => array(
 			'className' => 'Courses.CourseGrade',
@@ -42,23 +42,23 @@ class CourseGradeDetail extends CoursesAppModel {
 			'order' => ''
 		),
 	);
-	
-	public function beforeSave($options=array()) {
-		if(isset($this->data['CourseGradeDetail']['data']) && !empty($this->data['CourseGradeDetail']['data'])) {
+
+	public function beforeSave($options = array()) {
+		if (isset($this->data['CourseGradeDetail']['data']) && !empty($this->data['CourseGradeDetail']['data'])) {
 			$this->data['CourseGradeDetail']['data'] = serialize($this->data['CourseGradeDetail']['data']);
-			for($i = 0 ; $i < count($result['CourseGradeDetail']['data']['assignmentcategory']) ; $i++) {
-				$result['CourseGradeDetail']['data']['assignmentcategory'][$i]['weight'] = $result['CourseGradeDetail']['data']['assignmentcategory'][$i]['weight']/100;
+			for ($i = 0; $i < count($result['CourseGradeDetail']['data']['assignmentcategory']); $i++) {
+				$result['CourseGradeDetail']['data']['assignmentcategory'][$i]['weight'] = $result['CourseGradeDetail']['data']['assignmentcategory'][$i]['weight'] / 100;
 			}
 		}
 		parent::beforeSave($options);
 		return true;
 	}
-	
+
 	public function afterFind($results, $primary) {
 		foreach ($results as $index => $result) {
-			if(isset($result['CourseGradeDetail']['data']) && !empty($result['CourseGradeDetail']['data'])) {
+			if (isset($result['CourseGradeDetail']['data']) && !empty($result['CourseGradeDetail']['data'])) {
 				$result['CourseGradeDetail']['data'] = unserialize(($result['CourseGradeDetail']['data']));
-				for($i = 0 ; $i < count($result['CourseGradeDetail']['data']['assignmentcategory']) ; $i++) {
+				for ($i = 0; $i < count($result['CourseGradeDetail']['data']['assignmentcategory']); $i++) {
 					$result['CourseGradeDetail']['data']['assignmentcategory'][$i]['droplowest'] = (int) $result['CourseGradeDetail']['data']['assignmentcategory'][$i]['droplowest'];
 					$result['CourseGradeDetail']['data']['assignmentcategory'][$i]['allowcurve'] = (int) $result['CourseGradeDetail']['data']['assignmentcategory'][$i]['allowcurve'];
 					$result['CourseGradeDetail']['data']['assignmentcategory'][$i]['weight'] = (float) $result['CourseGradeDetail']['data']['assignmentcategory'][$i]['weight'];
@@ -66,23 +66,24 @@ class CourseGradeDetail extends CoursesAppModel {
 				$results[$index] = $result;
 			}
 		}
-		
+
 		return $results;
 	}
-	
+
 	public function gettypes($model, $id) {
 		$types = $this->find('first', array(
 			'conditions' => array(
 				'model' => $model,
 				'foreign_key' => $id
 		)));
-		
-		if(!$types) {
+
+		if (!$types) {
 			return array();
 		}
-		
+
 		$types = Hash::extract($types, 'CourseGradeDetail.data.assignmentcategory.{n}.type');
 		$types = array_combine($types, $types);
 		return $types;
 	}
+
 }
